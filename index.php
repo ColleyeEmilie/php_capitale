34c3a1f47bda924a76f897bf49761142a8372709
emilie <colleyeemilie@hotmail.com>
2023-02-09 12:30:39 +0100

    init

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$title = "Capitales";
+$countries = [
+    ["france" => [
+        "capital-name" => "paris",
+        "flag-file" => "france.png"]
+    ],
+    ["belgium" => [
+        "capital-name" => "bruxelles",
+        "flag-file" => "belgium.png"]
+    ],
+];
+
+require('index.view.php');
