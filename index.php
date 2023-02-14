aac34f9c91d7a10cdd5a38d439acf46c47b55f15
emilie <colleyeemilie@hotmail.com>
2023-02-14 19:40:13 +0100

    init

--- index.php
+++ index.php
@@ -1,14 +1,37 @@
 <?php
-$title = "Capitales";
+$title = "Choisis un pays, je te donnerai sa capitale";
 $countries = [
-    ["france" => [
-        "capital-name" => "paris",
-        "flag-file" => "france.png"]
+    'afrique du sud' => [
+        'capital-name' => 'johannesbourg',
+        'flag' => 'south-africa-flag-icon-256.png'
     ],
-    ["belgium" => [
-        "capital-name" => "bruxelles",
-        "flag-file" => "belgium.png"]
+    'belgique' => [
+        'capital-name' => 'bruxelles',
+        'flag' => 'belgium-flag-icon-256.png'
+    ],
+    'allemagne' => [
+        'capital-name' => 'berlin',
+        'flag' => 'germany-flag-icon-256.png'
+    ],
+    'corée du nord' => [
+        'capital-name' => 'pyongyang',
+        'flag' => 'north-korea-flag-icon-256.png'
     ],
 ];
+
+
+ksort($countries);
+$countryInfos = [];
+$requestedCountry = '';
+$errors = [];
+
+if (isset($_GET['country'])) {
+    $requestedCountry = urldecode($_GET['country']);
+    if(array_key_exists($requestedCountry, $countries)){
+        $countryInfos = $countries[$requestedCountry];
+    }else{
+        $errors['inexistent-country'] = 'ce pays n existe pas dans nos listes';
+    }
+}
 
 require('index.view.php');
--- index.view.php
+++ index.view.php
@@ -8,7 +8,44 @@
     <title>Capitale</title>
 </head>
 <body>
-    <h1><?php echo $title ?>
+<body>
+<main>
+    <h1><?= $title?></h1>
+    <form action="index.php">
+        <div >
+            <label for="countries">Les pays disponibles : </label>
+            <select  name="country">
+                <?php foreach ($countries as $countryName => $infos): ?>
+                    <option value="<?= urlencode($countryName); ?>"
+                        <?= $requestedCountry === $countryName ? ' selected' : '' ?>
+                    ><?= mb_strtoupper($countryName) ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        <div >
+            <button type="submit" >Donne moi sa capitale</button>
+        </div>
+    </form>
+    <?php if (isset($errors['inexistent-country'])): ?>
+        <!-- En cas d’erreur -->
+        <section class="alert alert-danger" role="alert">
+            <h2 class="text-center mb-4">⚠️ Attention&nbsp;! ⚠️</h2>
+            <p><?= $errors['inexistent-country']?></p>
+            <p>Merci d’en choisir un à l’aide du menu de sélection ci-dessus ☝🏼</p>
+        </section>
+    <?php endif; ?>
+    <?php if (count($countryInfos)): ?>
+        <section class="media">
+            <img src="images/<?= $countryInfos['flag'] ?>"
+                 alt="Drapeau de <?= ucwords($requestedCountry) ?>">
+            <div class="media-body">
+                <h2><?= ucwords($requestedCountry) ?></h2>
+                <p>Sa capitale est <?= ucwords($requestedCountry) ?></p>
+            </div>
+        </section>
+    <?php endif; ?>
+</main>
+</body>
 
 </body>
 </html>
